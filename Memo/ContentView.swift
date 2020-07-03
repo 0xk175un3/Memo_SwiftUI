deffb67d8889c7a519a3dff30f7750a9be544d80
0xk175un3 <artemiy@hey.com>
2020-07-03 05:30:46 +0300

    Lecture 1

--- Memo/ContentView.swift
+++ Memo/ContentView.swift
@@ -9,9 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!").padding()
+        HStack {
+            ForEach(0..<4) {
+                index in
+                CardView(isFaceUp: true)
+            }
+        }
+        .padding()
+        .foregroundColor(.orange)
+        .font(.largeTitle)
     }
 }
+
+struct CardView: View {
+    var isFaceUp: Bool
+    
+    var body: some View {
+        ZStack {
+            if isFaceUp {
+                RoundedRectangle(cornerRadius: 25.0).fill(Color.white)
+                RoundedRectangle(cornerRadius: 25.0).stroke(lineWidth: 3.0)
+                Text("👻")
+            }
+        }
+    }
+        }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
